2cfcb65c75692c9f0aa7c342ec3f66eae3463f86
siongriffiths <sig2@aber.ac.uk>
2013-11-04 20:07:00 +0000

    initial commit

---
+++ 8puzzle/src/BFS.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: pooey
+ * Date: 04/11/13
+ * Time: 20:05
+ * To change this template use File | Settings | File Templates.
+ */
+public class BFS {
+}
---
+++ 8puzzle/src/DFS.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: pooey
+ * Date: 04/11/13
+ * Time: 20:06
+ * To change this template use File | Settings | File Templates.
+ */
+public class DFS {
+}
