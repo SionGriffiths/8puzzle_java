9051c39f006cfe01fc51a00f86002dbbf109e2fa
SionGriffiths <sig2@aber.ac.uk>
2013-11-10 14:29:22 +0000

    Switch states to strings

--- 8puzzle/src/mainCode/BFS.java
+++ 8puzzle/src/mainCode/BFS.java
@@ -1,5 +1,6 @@
 package mainCode;
 
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -16,14 +17,39 @@ public class BFS {
   /**
    * Queue implementation using LinkedList
    */
-  private Queue<char []> q = new LinkedList<char[]>();
+  private Queue<String> queue = new LinkedList<String>();
 
+  /**
+   * HashSet implementation for the explored list. HashSet chosen to avoid duplicate states being parsed.
+   */
+  private HashSet<String> open = new HashSet<String>();
 
-
+  /**
+   * Constructs a BFS instance takes the Board as argument so current and goal states can be retrieved
+   * @param b the Board instance
+   */
   public BFS(Board b){
     this.b = b;
   }
 
+  /**
+   * Method to add states to the queue, checks if the state is already explored using the
+   * fact a Hashset will not allow duplicate entires
+   * @param state the state to be added
+   */
+  public void add(String state){
+    if(!open.contains(state)){
+      open.add(state);
+      queue.add(state);
+    }
+  }
 
+  public void runBFS(){
+    while(!queue.isEmpty()){
+      if(b.moveDown()){
+        add(b.getCurrentState());
+      }
+    }
+  }
 
 }
